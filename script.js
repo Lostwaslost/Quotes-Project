bf9f670065669dea621147a58d8e09fabba0b8fd
Lostwaslost <agastingoh@gmail.com>
2020-10-23 18:40:53 +0800

    googwrk

---
+++ script.js
@@ -0,0 +1,17 @@
+const quotes = [
+    {
+        name:'Stephen King',
+        quote:'Get busy living or get busy dying.'
+    },
+    {
+        name:'John F Kennedy',
+        quote:'Those we dare to fail miserably can achieve greatly.'
+    },{
+        name:'Abraham Lincoln',
+        quote:'I am a success today because I had a friend who beleived in me and I did not have the heart to let him down.'
+    },
+    {
+        name:'Leonardo Da Vinci', 
+        
+    }
+]
